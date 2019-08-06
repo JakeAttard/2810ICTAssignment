125a70ba0699263b4f75a328d561cc553a495f0d
Jake Attard <jakeattard18@gmail.com>
2019-08-06 14:13:32 +1000

    Changes to the word_ladder.py file

--- word_ladder.py
+++ word_ladder.py
@@ -19,7 +19,7 @@ def dictionaryListFile():
     file = open(checkUserInput("Enter dictionary file: ", checkInput = False))
   except:
     print("The dictionary file provided does not exist.")
-    exit(0) ### Test with no 0
+    exit()
   
   fileLines = file.readlines()
   for fileLine in range(len(fileLines)):
@@ -28,33 +28,33 @@ def dictionaryListFile():
       fileLines.pop(fileLine)
   if len(fileLines) == 0:
     print("The file is empty.")
-    exit(0)
+    exit()
   return fileLines
 
 # Same function
 def same(item, target):
-  return len([c for (c, t) in zip(item, target) if c == t])
+  return len([itemLetter for (itemLetter, targetLetter) in zip(item, target) if itemLetter == targetLetter])
 
 #Build Function
-def build(pattern, words, seen, list):
-  return [word for word in words 
-    if re.search(pattern, word) and word not in seen.keys() and word not in list]
+def build(pattern, words, seen, potentialNextWords):
+  return [word for word in words if re.search(pattern, word) and word not in seen.keys() and word not in potentialNextWords]
 
 #Finding word function
 def find(word, words, seen, target, path):
-  list = []
-  for i in range(len(word)):
-    list += build(word[:i] + "." + word[i + 1:], words, seen, list)
-  if len(list) == 0:
+  potentialNextWords = []
+  fixedIndexes=[i for i in range(len(word)) if word[i] == target[i]]
+  for i in [index for index in range(len(word)) if index not in fixedIndexes]:
+    potentialNextWords += build(word[:i] + "." + word[i + 1:], words, seen, potentialNextWords)
+  if len(potentialNextWords) == 0:
     return False
-  list = sorted([(same(w, target), w) for w in list])
-  for (match, item) in list:
+  potentialNextWords = sorted([(same(word, target), word) for word in potentialNextWords], reverse=True)
+  for (match, item) in potentialNextWords:
     if match >= len(target) - 1:
       if match == len(target) - 1:
         path.append(item)
       return True
     seen[item] = True
-  for (match, item) in list:
+  for (match, item) in potentialNextWords:
     path.append(item)
     if find(item, words, seen, target, path):
       return True
@@ -67,7 +67,7 @@ startWord = {}
 while True:
   excludeWords = input("Do you want to exclude any words? Please type Y to continue and N to skip. ").lower()
   if excludeWords != "y" and excludeWords != "n":
-    print("Please type 'Y' or 'N' ")
+    print("Please type 'y' or 'n' ")
     continue
   if excludeWords == "y":
     excludeWords = dictionaryListFile()
@@ -97,3 +97,35 @@ while True:
     print("The dictionary word inputed can't be found")
     continue
   break
+
+# Check if this works
+words = [word for word in dicionaryList if len(word) == len(start)]
+
+shortestPath = True
+
+if shortestPath:
+  minPath = None
+
+while True:
+  path = [start]
+  seen = startWord.copy()
+  pathfound = find(start, words, seen, target, path)
+
+  if pathfound and not shortestPath:
+    path.append(target)
+    print(len(path) -1, " >> ".join(path))
+  elif shortestPath and pathfound:
+    path.append(target)
+    if pathfound and (minPath == None or len(path) -1 < len(minPath)):
+      minPath = path
+  elif not pathfound:
+    if shortestPath:
+      if minPath == None:
+        print("No path 1 found")
+        break
+      print(len(minPath) - 1, " >> ".join(minPath))
+    else:
+      print("No path 2 found")
+    break
+  for word in path:
+    startWord[word] = True
