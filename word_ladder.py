4ca035673ae4ef076f901a7a7bef1c7af9bd534a
Steven <stevenphan10@gmail.com>
2019-08-20 10:19:23 +1000

    Refactoring the word_ladder gram program.

--- word_ladder.py
+++ word_ladder.py
@@ -6,44 +6,6 @@
 import re
 print("Welcome to the ladder-gram program!")
 
-#DictionaryListFile Function
-def dictionaryListFile():
-  try:
-    dictionaryFileName = input("Enter dictionary file name: ")
-    file = open(dictionaryFileName)
-  except:
-    print("The dictionary file can't be found. Please check if you have the correct .txt file.")
-    exit("Please re-run the program and try again!")
-  
-  dictionaryFileLines = file.readlines()
-  for dictionaryFileLine in range(len(dictionaryFileLines)):
-    dictionaryFileLines[dictionaryFileLine] = dictionaryFileLines[dictionaryFileLine].strip()
-    if dictionaryFileLines[dictionaryFileLine] == "":
-      dictionaryFileLines.pop(dictionaryFileLine)
-  if len(dictionaryFileLines) == int(0):
-    print("The dictionary file you entered is empty.")
-    exit("Please re-run the program and try again!")
-  return dictionaryFileLines
-
-# ExcludedFile function for the additional functionality
-def excludedListFile():
-  try:
-    excludedFileName = input("Enter your excluded file name: ")
-    file = open(excludedFileName)
-  except:
-    print("The excluded file can't be found. Please check if you have the correct .txt file.")
-    exit("Please re-run the program and try again!")
-
-  excludedFileLines = file.readlines()
-  for excludedFileLine in range(len(excludedFileLines)):
-    excludedFileLines[excludedFileLine] = excludedFileLines[excludedFileLine].strip()
-    if excludedFileLines[excludedFileLine] == "":
-      excludedFileLines.pop(excludedFileLine)
-  if len(excludedFileLines) == int(0):
-    print("The excluded file you entered is empty.")
-    exit("Please re-run the program and try again!")
-  return excludedFileLines
-  
 # Same function
 # Original code from the word_ladder given just renamed variables from c and t to better naming conventions
 def same(item, target):
@@ -78,6 +40,44 @@ def find(word, words, seen, target, path):
     if find(item, words, seen, target, path):
       return bool(True)
     path.pop()
+
+#DictionaryListFile Function
+def dictionaryListFile():
+  try:
+    dictionaryFileName = input("Enter dictionary file name: ")
+    file = open(dictionaryFileName)
+  except:
+    print("The dictionary file can't be found. Please check if you have the correct .txt file.")
+    exit("Please re-run the program and try again!")
+    
+  dictionaryFileLines = file.readlines()
+  for dictionaryFileLine in range(len(dictionaryFileLines)):
+    dictionaryFileLines[dictionaryFileLine] = dictionaryFileLines[dictionaryFileLine].strip()
+    if dictionaryFileLines[dictionaryFileLine] == "":
+      dictionaryFileLines.pop(dictionaryFileLine)
+  if len(dictionaryFileLines) == int(0):
+    print("The dictionary file you entered is empty.")
+    exit("Please re-run the program and try again!")
+  return dictionaryFileLines
+
+# ExcludedFile function for the additional functionality
+def excludedListFile():
+  try:
+    excludedFileName = input("Enter your excluded file name: ")
+    file = open(excludedFileName)
+  except:
+    print("The excluded file can't be found. Please check if you have the correct .txt file.")
+    exit("Please re-run the program and try again!")
+
+  excludedFileLines = file.readlines()
+  for excludedFileLine in range(len(excludedFileLines)):
+    excludedFileLines[excludedFileLine] = excludedFileLines[excludedFileLine].strip()
+    if excludedFileLines[excludedFileLine] == "":
+      excludedFileLines.pop(excludedFileLine)
+  if len(excludedFileLines) == int(0):
+    print("The excluded file you entered is empty.")
+    exit("Please re-run the program and try again!")
+  return excludedFileLines
 
 # DictionaryList now has access to the dictionaryListFile() function
 dictionaryList = dictionaryListFile()
